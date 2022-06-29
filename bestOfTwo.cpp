b951513f6880fbdbbe536cb144586cca2cef784a
Aman Shankar Singh <amanshankarsingh05@gmail.com>
2022-06-29 23:58:51 +0530

    First commit

---
+++ bestOfTwo.cpp
@@ -0,0 +1,17 @@
+//Problem name "BESTOFTWO"
+//Problem code "BESTOFTWO"
+
+#include"bits/stdc++.h"
+using namespace std;
+int main(){
+   int t;cin>>t;
+   while(t--){
+      int x,y;cin>>x>>y;
+      if(x>=y){
+         cout<<x<<endl;
+      }
+      else{
+         cout<<y<<endl;
+      }
+   }
+}
