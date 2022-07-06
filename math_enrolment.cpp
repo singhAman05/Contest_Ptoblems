bdf9f4bbaccf1b550f6b722fc11d181dd4178671
Aman Shankar Singh <amanshankarsingh05@gmail.com>
2022-07-06 23:13:12 +0530

    nineth commit

---
+++ math_enrolment.cpp
@@ -0,0 +1,17 @@
+//Problem code:- M1ENROL
+
+#include"bits/stdc++.h"
+using namespace std;
+int main(){
+   int t;cin>>t;
+   while(t--){
+      int x,y;cin>>x>>y;
+      if(x>y){
+         cout<<"0"<<endl;
+      }
+      else{
+         cout<<y-x<<endl;
+      }
+   }
+   return 0;
+}
