f54d1c006b23dd52ceca1e3375cb20ad6e045247
Aman Shankar Singh <amanshankarsingh05@gmail.com>
2022-07-06 23:09:47 +0530

    sixth commit

---
+++ counting_wrds.cpp
@@ -0,0 +1,12 @@
+//Problem code:- CNTWRD
+
+#include"bits/stdc++.h"
+using namespace std;
+int main(){
+   int t;cin>>t;
+   while(t--){
+      int n,m;cin>>n>>m;
+      cout<<n*m<<endl;
+   }
+   return 0;
+}
