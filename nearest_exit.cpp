e35f51d2a7e1e2525724e106e36e8514f17f7305
Aman <amanshankarsingh05@gmail.com>
2022-09-16 00:17:51 +0530

    initial commit

---
+++ nearest_exit.cpp
@@ -0,0 +1,12 @@
+//NEAREST EXIT
+#include"bits/stdc++.h"
+using namespace std;
+long long main(){
+    long long t;cin>>t;
+    while(t--){
+        long long x;cin>>x;
+        if(x<=50 && x>=1) cout<<"LEFT\n";
+        else cout<<"RIGHT\n";
+    }
+    return 0;
+}
